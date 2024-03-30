124c18478ad65d8f455a860fae83195976e4c1c8
taewoongjung <taewj@howser.co.kr>
2024-03-31 03:28:33 +0900

    cors 수정

--- src/main/java/com/interior/adapter/config/security/CorsMvcConfig.java
+++ src/main/java/com/interior/adapter/config/security/CorsMvcConfig.java
@@ -15,7 +15,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
         corsRegistry.addMapping("/**")
                 .allowCredentials(true)
-                .exposedHeaders("Authorization")
+                .exposedHeaders("*")
                 .allowedOrigins(frontOriginUrl);
     }
 
