5476151b43022353bf858e537c66f5f2d79c8f25
taewoongjung <taewj@howser.co.kr>
2024-04-05 11:14:56 +0900

    cors 설정 수정

--- src/main/java/com/interior/adapter/config/security/CorsMvcConfig.java
+++ src/main/java/com/interior/adapter/config/security/CorsMvcConfig.java
@@ -2,6 +2,7 @@ package com.interior.adapter.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,10 +11,13 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Value("${server.front.origin-url}")
     private String frontOriginUrl;
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION)
                 .allowedOrigins(frontOriginUrl);
     }
 
