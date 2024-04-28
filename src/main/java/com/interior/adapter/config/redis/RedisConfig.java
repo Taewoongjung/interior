caaf10fbbe5ef40634946a1530c18b0a7a49de71
taewoongjung <taewj@howser.co.kr>
2024-04-29 00:52:21 +0900

    defined in URL 에러 해결

--- src/main/java/com/interior/adapter/config/redis/RedisConfig.java
+++ src/main/java/com/interior/adapter/config/redis/RedisConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -16,7 +15,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Getter
-@Configuration
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
