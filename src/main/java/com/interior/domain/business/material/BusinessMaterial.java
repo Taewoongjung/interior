aba0efd8aa4ce4d3c4633936d83203d0f49520bf
taewoongjung <taewj@howser.co.kr>
2024-05-04 21:45:04 +0900

    특정 사업의 모든 재료 조회 API 응답값에 필드 추가
    - allMaterialCostPerUnit
    - allLaborCostPerUnit
    - totalUnitPrice
    - totalPrice

--- src/main/java/com/interior/domain/business/material/BusinessMaterial.java
+++ src/main/java/com/interior/domain/business/material/BusinessMaterial.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.interior.domain.business.expense.BusinessMaterialExpense;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.ToString;
@@ -31,11 +32,20 @@ public class BusinessMaterial {
 
     private BusinessMaterialExpense businessMaterialExpense;
 
+    private String allMaterialCostPerUnit;
+
+    private String allLaborCostPerUnit;
+
+    private String totalUnitPrice;
+
+    private String totalPrice;
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime lastModified;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
+
 
     private BusinessMaterial(
             final Long id,
@@ -47,6 +57,10 @@ public class BusinessMaterial {
             final String unit,
             final String memo,
             final BusinessMaterialExpense businessMaterialExpense,
+            final String allMaterialCostPerUnit,
+            final String allLaborCostPerUnit,
+            final String totalUnitPrice,
+            final String totalPrice,
             final LocalDateTime lastModified,
             final LocalDateTime createdAt
     ) {
@@ -59,6 +73,10 @@ public class BusinessMaterial {
         this.unit = unit;
         this.memo = memo;
         this.businessMaterialExpense = businessMaterialExpense;
+        this.allMaterialCostPerUnit = allMaterialCostPerUnit;
+        this.allLaborCostPerUnit = allLaborCostPerUnit;
+        this.totalUnitPrice = totalUnitPrice;
+        this.totalPrice = totalPrice;
         this.lastModified = lastModified;
         this.createdAt = createdAt;
     }
@@ -74,6 +92,34 @@ public class BusinessMaterial {
             final String memo,
             final BusinessMaterialExpense businessMaterialExpense
     ) {
+
+        String allMaterialCostPerUnit = null;
+        String allLaborCostPerUnit = null;
+        String totalUnitPrice = null;
+        String totalPrice = null;
+
+        if (businessMaterialExpense != null) {
+
+            BigDecimal materialCostPerUnit = new BigDecimal(
+                    businessMaterialExpense.getMaterialCostPerUnit());
+
+            // 총 재료비 단가
+            allMaterialCostPerUnit = setAllAmountOfSpecificExpense(amount, materialCostPerUnit).toPlainString();
+
+            BigDecimal laborCostPerUnit = new BigDecimal(
+                    businessMaterialExpense.getLaborCostPerUnit());
+
+            // 총 노무비 단가
+            allLaborCostPerUnit = setAllAmountOfSpecificExpense(amount, laborCostPerUnit).toPlainString();
+
+            // 총 단가
+            BigDecimal totalUnitPriceBigDecimal = materialCostPerUnit.add(laborCostPerUnit);
+            totalUnitPrice = removeDecimal(totalUnitPriceBigDecimal).toPlainString();
+
+            // 총 금액
+            totalPrice = removeDecimal(totalUnitPriceBigDecimal.multiply(amount)).toPlainString();
+        }
+
         return new BusinessMaterial(
                 id,
                 businessId,
@@ -84,7 +130,29 @@ public class BusinessMaterial {
                 unit,
                 memo,
                 businessMaterialExpense,
+                allMaterialCostPerUnit, allLaborCostPerUnit,
+                totalUnitPrice, totalPrice,
                 LocalDateTime.now(), LocalDateTime.now());
+    }
+
+    private static BigDecimal setAllAmountOfSpecificExpense(final BigDecimal amount,
+            final BigDecimal targetCostPerUnit) {
+
+        BigDecimal totalTargetCostPerUnit = amount.multiply(targetCostPerUnit);
+
+        return removeDecimal(totalTargetCostPerUnit);
+    }
+
+    private static BigDecimal removeDecimal(final BigDecimal target) {
+
+        BigDecimal response = null;
+
+        // 결과가 소수점 이하 모두 0인지 확인
+        if (target.scale() <= 0 || target.stripTrailingZeros().scale() <= 0) {
+            // 소수점 이하 모두 0이면 소수점 제거
+            response = target.setScale(0, RoundingMode.UNNECESSARY);
+        }
+        return response;
     }
 
     public static BusinessMaterial of(
@@ -106,6 +174,8 @@ public class BusinessMaterial {
                 unit,
                 memo,
                 null,
+                null, null,
+                null,null,
                 LocalDateTime.now(), LocalDateTime.now());
     }
 }
