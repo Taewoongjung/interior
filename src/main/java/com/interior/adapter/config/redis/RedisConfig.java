ad973786ecc769e800b8ad43b0cbe9ac94e610be
taewoongjung <taewj@howser.co.kr>
2024-06-05 22:03:15 +0900

    CLN: redis 설정 수정

--- src/main/java/com/interior/adapter/config/redis/RedisConfig.java
+++ src/main/java/com/interior/adapter/config/redis/RedisConfig.java
@@ -25,7 +25,7 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(host, 6379);
     }
 
     @Bean
