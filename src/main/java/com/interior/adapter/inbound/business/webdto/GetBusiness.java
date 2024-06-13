34f1943fb3f6e44b2e83b15a30f18e2bf4a870e5
taewoongjung <taewj@howser.co.kr>
2024-06-13 22:08:22 +0900

    CLN: 특정 사업의 모든 재료 조회 응답값에 businessCreatedAt 포멧 추가

--- src/main/java/com/interior/adapter/inbound/business/webdto/GetBusiness.java
+++ src/main/java/com/interior/adapter/inbound/business/webdto/GetBusiness.java
@@ -1,5 +1,6 @@
 package com.interior.adapter.inbound.business.webdto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.interior.domain.business.material.BusinessMaterial;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ public class GetBusiness {
 
     public record Response(
             String businessName,
+            @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
             LocalDateTime businessCreatedAt,
             HashMap<String, List<BusinessMaterial>> businessMaterials,
             int count
