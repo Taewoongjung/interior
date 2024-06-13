d995767a96728e6ddae826b13f4319c9b249fe9a
taewoongjung <taewj@howser.co.kr>
2024-06-13 17:23:23 +0900

    ENH: #TW-37 BusinessContract, BusinessProgress 각각 테이블 + domain entity, jpa entity 추가

--- src/main/java/com/interior/adapter/outbound/jpa/entity/business/progress/BusinessProgressEntity.java
+++ src/main/java/com/interior/adapter/outbound/jpa/entity/business/progress/BusinessProgressEntity.java
@@ -1,4 +1,4 @@
-package com.interior.adapter.outbound.jpa.entity.business.contract;
+package com.interior.adapter.outbound.jpa.entity.business.progress;
 
 import com.interior.domain.util.BoolType;
 import jakarta.persistence.Column;
@@ -19,30 +19,20 @@ import org.springframework.data.annotation.CreatedDate;
 @Entity
 @Getter
 @ToString
-@Table(name = "business_contract")
+@Table(name = "business_progress")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BusinessContractEntity {
+public class BusinessProgressEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "company_id", nullable = false, columnDefinition = "bigint")
-    private Long companyId;
-
     @Column(name = "business_id", nullable = false, columnDefinition = "bigint")
     private Long businessId;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "contract_type", nullable = false, columnDefinition = "varchar(50)")
-    private ContractType contractType;
-
-    @Column(name = "is_agreed", columnDefinition = "varchar(1)")
-    @Enumerated(value = EnumType.STRING)
-    private BoolType isAgreed;
-
-    @Column(name = "user_id", nullable = false, columnDefinition = "bigint")
-    private Long userId;
+    @Column(name = "progress_type", nullable = false, columnDefinition = "varchar(50)")
+    private ProgressType progressType;
 
     @Column(name = "is_deleted", columnDefinition = "varchar(1)")
     @Enumerated(value = EnumType.STRING)
@@ -52,50 +42,38 @@ public class BusinessContractEntity {
     @Column(nullable = false, updatable = false, name = "created_at")
     private LocalDateTime createdAt;
 
-    private BusinessContractEntity(
+    public BusinessProgressEntity(
             final Long id,
-            final Long companyId,
             final Long businessId,
-            final ContractType contractType,
-            final BoolType isAgreed,
-            final Long userId,
+            final ProgressType progressType,
             final BoolType isDeleted
     ) {
 
         this.id = id;
-        this.companyId = companyId;
         this.businessId = businessId;
-        this.contractType = contractType;
-        this.isAgreed = isAgreed;
-        this.userId = userId;
+        this.progressType = progressType;
         this.isDeleted = isDeleted;
         this.createdAt = LocalDateTime.now();
     }
 
     // 생성
-    public static BusinessContractEntity of(
-            final Long companyId,
+    public static BusinessProgressEntity of(
             final Long businessId,
-            final ContractType contractType,
-            final BoolType isAgreed,
-            final Long userId,
+            final ProgressType progressType,
             final BoolType isDeleted
     ) {
-        return new BusinessContractEntity(null, companyId, businessId, contractType, isAgreed,
-                userId, isDeleted);
+
+        return new BusinessProgressEntity(null, businessId, progressType, isDeleted);
     }
 
     // 조회
-    public static BusinessContractEntity of(
+    public static BusinessProgressEntity of(
             final Long id,
-            final Long companyId,
             final Long businessId,
-            final ContractType contractType,
-            final BoolType isAgreed,
-            final Long userId,
+            final ProgressType progressType,
             final BoolType isDeleted
     ) {
-        return new BusinessContractEntity(id, companyId, businessId, contractType, isAgreed,
-                userId, isDeleted);
+
+        return new BusinessProgressEntity(id, businessId, progressType, isDeleted);
     }
 }
