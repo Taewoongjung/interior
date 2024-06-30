9569ee400ef92abbf8886445207d05a6886858f3
taewoongjung <taewj@howser.co.kr>
2024-06-30 15:47:43 +0900

    CLN: #TW-44 command,query controller 분리

--- src/main/java/com/interior/adapter/inbound/business/command/BusinessCommandController.java
+++ src/main/java/com/interior/adapter/inbound/business/command/BusinessCommandController.java
@@ -1,9 +1,8 @@
-package com.interior.adapter.inbound.business;
+package com.interior.adapter.inbound.business.command;
 
-import com.interior.adapter.inbound.business.enumtypes.QueryType;
 import com.interior.adapter.inbound.business.webdto.CreateBusinessMaterial.CreateBusinessMaterialReqDto;
 import com.interior.adapter.inbound.business.webdto.CreateBusinessWebDtoV1;
-import com.interior.adapter.inbound.business.webdto.GetBusiness;
+import com.interior.adapter.inbound.business.webdto.CreateBusinessWebDtoV1.Res;
 import com.interior.adapter.inbound.business.webdto.ReviseBusiness;
 import com.interior.adapter.inbound.business.webdto.ReviseBusinessMaterialWebDtoV1;
 import com.interior.adapter.inbound.business.webdto.ReviseUsageCategoryOfMaterial;
@@ -27,36 +26,22 @@ import com.interior.application.command.business.handlers.ReviseMaterialCommandH
 import com.interior.application.command.business.handlers.ReviseUsageCategoryOfMaterialCommandHandler;
 import com.interior.application.command.business.handlers.SendQuotationDraftToClientCommandHandler;
 import com.interior.application.command.business.handlers.UpdateBusinessProgressCommandHandler;
-import com.interior.application.readmodel.business.dto.GetBusinessMaterialLogs;
-import com.interior.application.readmodel.business.temp.BusinessQueryService;
-import com.interior.domain.business.Business;
-import com.interior.domain.business.material.log.BusinessMaterialLog;
-import com.interior.domain.company.Company;
 import com.interior.domain.user.User;
-import jakarta.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
-public class BusinessController {
-
-    private final BusinessQueryService businessQueryService;
+public class BusinessCommandController {
 
     private final CreateBusinessCommandHandler createBusinessCommandHandler;
     private final CreateBusinessMaterialCommandHandler createBusinessMaterialCommandHandler;
@@ -70,7 +55,7 @@ public class BusinessController {
 
     // 사업 추가
     @PostMapping(value = "/api/companies/{companyId}/businesses")
-    public ResponseEntity<CreateBusinessWebDtoV1.Res> createBusiness(
+    public ResponseEntity<Res> createBusiness(
             @PathVariable(value = "companyId") final Long companyId,
             @RequestBody final CreateBusinessWebDtoV1.Req req,
             @AuthenticationPrincipal final User user
@@ -121,14 +106,37 @@ public class BusinessController {
                         new DeleteBusinessMaterialCommand(businessId, materialId, user)));
     }
 
-    // 특정 사업의 모든 재료 조회
-    @GetMapping(value = "/api/businesses/{businessId}")
-    public ResponseEntity<GetBusiness.Response> getBusiness(
-            @PathVariable(value = "businessId") final Long businessId
+    // 사업 삭제
+    @DeleteMapping(value = "/api/companies/{companyId}/businesses/{businessId}")
+    public ResponseEntity<Boolean> deleteBusiness(
+            @PathVariable(value = "companyId") final Long companyId,
+            @PathVariable(value = "businessId") final Long businessId,
+            @AuthenticationPrincipal final User user
     ) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(businessQueryService.getBusiness(businessId));
+                .body(deleteBusinessCommandHandler.handle(
+                        new DeleteBusinessCommand(companyId, businessId, user)));
     }
+
+    // 사업 수정
+    @PatchMapping(value = "/api/companies/{companyId}/businesses/{businessId}")
+    public ResponseEntity<Boolean> reviseBusiness(
+            @PathVariable(value = "companyId") final Long companyId,
+            @PathVariable(value = "businessId") final Long businessId,
+            @RequestBody final ReviseBusiness.WebReqV1 req,
+            @AuthenticationPrincipal final User user
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reviseBusinessCommandHandler.handle(
+                        new ReviseBusinessCommand(
+                                companyId,
+                                businessId,
+                                req.changeBusinessName(),
+                                user
+                        ))
+                );
+    }
+
 
     // 특정 사업 재료의 사업 분류 수정
     @PatchMapping(value = "/api/businesses/{businessId}/categories/constructions")
@@ -162,98 +170,6 @@ public class BusinessController {
                 );
     }
 
-    // 유저의 모든 사업들 조회
-    @GetMapping(value = "/api/businesses")
-    public ResponseEntity<List<Business>> getBusinessByUser(
-            @AuthenticationPrincipal final User user
-    ) {
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(businessQueryService.getAllBusinessesByUser(
-                        user.getCompanyList().stream()
-                                .map(Company::getId)
-                                .toList()));
-    }
-
-    // 회사의 사업들 조회
-    @GetMapping(value = "/api/companies/{companyId}/businesses")
-    public ResponseEntity<List<Business>> getBusinessByCompanyId(
-            @PathVariable(value = "companyId") final Long companyId,
-            @RequestParam(value = "queryType", defaultValue = "none") final QueryType queryType
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(businessQueryService.getBusinessesByCompanyId(companyId, queryType));
-    }
-
-    // 사업 삭제
-    @DeleteMapping(value = "/api/companies/{companyId}/businesses/{businessId}")
-    public ResponseEntity<Boolean> deleteBusiness(
-            @PathVariable(value = "companyId") final Long companyId,
-            @PathVariable(value = "businessId") final Long businessId,
-            @AuthenticationPrincipal final User user
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(deleteBusinessCommandHandler.handle(
-                        new DeleteBusinessCommand(companyId, businessId, user)));
-    }
-
-    // 사업 수정
-    @PatchMapping(value = "/api/companies/{companyId}/businesses/{businessId}")
-    public ResponseEntity<Boolean> reviseBusiness(
-            @PathVariable(value = "companyId") final Long companyId,
-            @PathVariable(value = "businessId") final Long businessId,
-            @RequestBody final ReviseBusiness.WebReqV1 req,
-            @AuthenticationPrincipal final User user
-    ) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(reviseBusinessCommandHandler.handle(
-                        new ReviseBusinessCommand(
-                                companyId,
-                                businessId,
-                                req.changeBusinessName(),
-                                user
-                        ))
-                );
-    }
-
-    // 회사의 사업 리스트 엑셀 다운로드
-    @GetMapping(value = "/api/excels/companies/{companyId}/businesses/{businessId}")
-    public void getExcelOfBusinessMaterialList(
-            @PathVariable(value = "companyId") final Long companyId,
-            @PathVariable(value = "businessId") final Long businessId,
-            @RequestParam("taskId") final String taskId,
-            HttpServletResponse response
-    ) {
-        businessQueryService.getExcelOfBusinessMaterialList(companyId, businessId, taskId,
-                response);
-    }
-
-    // 회사의 사업 리스트 엑셀 다운로드 프로그레스 조회
-    @GetMapping(value = "/api/excels/tasks/{taskId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter getExcelOfBusinessMaterialListProgressInfo(
-            @PathVariable(value = "taskId") final String taskId
-    ) {
-        return businessQueryService.getExcelProgressInfo(taskId);
-    }
-
-    // 재료 변경 로그 조회
-    @GetMapping(value = "/api/businesses/{businessId}/logs")
-    public ResponseEntity<List<GetBusinessMaterialLogs.Res>> getBusinessMaterialLogs(
-            @PathVariable(value = "businessId") final Long businessId) {
-
-        List<BusinessMaterialLog> logList = businessQueryService.getBusinessMaterialLog(businessId);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(logList.stream()
-                        .map(e ->
-                                new GetBusinessMaterialLogs.Res(
-                                        e.getUpdaterName(),
-                                        e.getChangeField().getDesc(),
-                                        e.getChangeDetail(),
-                                        e.getCreatedAt()
-                                )
-                        ).collect(Collectors.toList()));
-    }
 
     // @TODO: 다른 사용자가 사업 단계 상태값을 악의적으로 변경할 가능성을 생각 해서 방안 찾아보기
     // 1. 관리자가 검수한다.
