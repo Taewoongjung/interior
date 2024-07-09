644294e90e1a920b1d094b27f95433a970397a91
taewoongjung <taewj@howser.co.kr>
2024-07-09 21:47:40 +0900

    TST: #TW-70 GetUsageCategoriesOfBusinessQueryHandlerTest 수정
    - GetUsageCategoriesOfBusinessQueryHandler 는 사업에 해당하는 모든 사업분류를 조회한다.

--- src/testFixtures/java/business/BusinessFixture.java
+++ src/testFixtures/java/business/BusinessFixture.java
@@ -1,12 +1,10 @@
 package business;
 
 import com.interior.domain.business.Business;
-import com.interior.domain.business.material.BusinessMaterial;
 import com.interior.domain.business.progress.BusinessProgress;
 import com.interior.domain.business.progress.ProgressType;
 import com.interior.domain.util.BoolType;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +51,7 @@ public class BusinessFixture {
             "01000", "부산 해운대구 APEC로 21", "401동 1202호", "2635010500115130000000002",
             LocalDateTime.of(2024, 7, 1, 2, 3),
             LocalDateTime.of(2024, 7, 1, 2, 3),
-            getBusinessMaterial(), getBusinessProgress());
+            new ArrayList<>(), getBusinessProgress());
 
     public static Business B_3 = Business.of(
             3L, "사업 현장 2", 56L, 519L, BoolType.T,
@@ -75,29 +73,6 @@ public class BusinessFixture {
 
         list.add(BusinessProgress.of(3L, ProgressType.IN_PROGRESS));
         list.add(BusinessProgress.of(3L, ProgressType.QUOTATION_REQUESTED));
-
-        return list;
-    }
-
-    private static List<BusinessMaterial> getBusinessMaterial() {
-
-        List<BusinessMaterial> list = new ArrayList<>();
-
-        list.add(BusinessMaterial.of(1L, 4L, "벽 타일",
-                "외벽 공사", "타일", BigDecimal.valueOf(1), "ea",
-                "memo", BoolType.F,
-                LocalDateTime.of(2024, 7, 1, 6, 3),
-                LocalDateTime.of(2024, 7, 1, 6, 3),
-                null
-        ));
-
-        list.add(BusinessMaterial.of(2L, 4L, "벽돌",
-                "외벽 공사", "돌", BigDecimal.valueOf(1),
-                "ea", "memo", BoolType.F,
-                LocalDateTime.of(2024, 7, 1, 6, 3),
-                LocalDateTime.of(2024, 7, 1, 6, 3),
-                null
-        ));
 
         return list;
     }
