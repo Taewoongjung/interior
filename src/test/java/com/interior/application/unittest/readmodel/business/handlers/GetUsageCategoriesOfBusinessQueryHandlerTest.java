15b3988e23360cd8b1215198f9af40fb489d2af3
taewoongjung <taewj@howser.co.kr>
2024-07-09 21:15:07 +0900

    TST: #TW-70 GetUsageCategoriesOfBusinessQueryHandlerTest
    - GetUsageCategoriesOfBusinessQueryHandler 는 사업에 해당하는 모든 사업분류를 조회한다.

---
+++ src/test/java/com/interior/application/unittest/readmodel/business/handlers/GetUsageCategoriesOfBusinessQueryHandlerTest.java
@@ -0,0 +1,37 @@
+package com.interior.application.unittest.readmodel.business.handlers;
+
+import com.interior.application.readmodel.business.handlers.GetUsageCategoriesOfBusinessQueryHandler;
+import com.interior.application.readmodel.business.queryresponses.GetUsageCategoriesOfBusinessQueryResponse;
+import com.interior.domain.business.repository.BusinessRepository;
+import com.interior.helper.spy.BusinessRepositorySpy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("GetUsageCategoriesOfBusinessQueryHandler 는 ")
+class GetUsageCategoriesOfBusinessQueryHandlerTest {
+
+    private final BusinessRepository businessRepository = new BusinessRepositorySpy();
+
+    private final GetUsageCategoriesOfBusinessQueryHandler sut = new GetUsageCategoriesOfBusinessQueryHandler(businessRepository);
+
+
+    @Test
+    @DisplayName("사업에 해당하는 모든 사업분류를 조회한다.")
+    void test1() {
+
+        // given
+        Long businessId = 4L;
+
+        // when
+        // then
+        List<GetUsageCategoriesOfBusinessQueryResponse> actual = sut.handle(businessId);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.size()).isEqualTo(1);
+        assertThat(actual.get(0).count()).isEqualTo(3);
+    }
+}
